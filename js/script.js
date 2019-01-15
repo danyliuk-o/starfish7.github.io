575f4beff2619fb2621bd63319a8594aec80d4aa
Alexander <starfishua@gmail.com>
2019-01-15 21:43:10 +0200

    solar update1

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$('.card').on('click', function() {
+$('.card').on('click', () => {
     $('.container').toggleClass('container-origin');
 });
 
@@ -7,8 +7,8 @@ $(document).ready(function() {
 
     $("body").fadeIn(1500);
 
-    $("a.transition").click(function(event){
-        event.preventDefault();
+    $("a.transition").click(e => {
+        e.preventDefault();
         linkLocation = this.href;
         $("body").fadeOut(1000, redirectPage);
     });
@@ -20,23 +20,22 @@ $(document).ready(function() {
 
 
 
-$("html").click(function(e){
+$("html").click(function(e) {
     var ripple = $(this);
     // визуальный элемент эффекта
-    if(ripple.find(".effekt").length == 0) {
+    if (ripple.find(".effekt").length == 0) {
         ripple.append("<span class='effekt'></span>");
     }
     var efekt = ripple.find(".effekt");
     efekt.removeClass("replay");
-    if(!efekt.height() && !efekt.width())
-    {
+    if (!efekt.height() && !efekt.width()) {
         var d = Math.max(ripple.outerWidth(), ripple.outerHeight());
-        efekt.css({height: d/5, width: d/5});// Определяем размеры элемента эффекта
+        efekt.css({ height: d / 5, width: d / 5 }); // Определяем размеры элемента эффекта
     }
-    var x = e.pageX - ripple.offset().left - efekt.width()/2;
-    var y = e.pageY - ripple.offset().top - efekt.height()/2;
+    var x = e.pageX - ripple.offset().left - efekt.width() / 2;
+    var y = e.pageY - ripple.offset().top - efekt.height() / 2;
     efekt.css({
-        top: y+'px',
-        left:x+'px'
+        top: y + 'px',
+        left: x + 'px'
     }).addClass("replay");
 })
